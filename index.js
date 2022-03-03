1bb22b395e404b24e6d2c3646a3eebd9dc37620c
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-03 15:17:46 +0300

    updated index.html

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // // document.getElementById("count-el").innerText = 10
 // //store data
+
+let bonusPoint = 50
+console.log(bonusPoint)
+
+bonusPoint =bonusPoint+50
+console.log(bonusPoint)
+
+bonusPoint = bonusPoint- 75
+console.log(bonusPoint)
+
+bonusPoint = bonusPoint + 45
+console.log(bonusPoint)
