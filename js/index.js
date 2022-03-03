316007817cc4c8c95f0b3dde42a2ea9319a5c232
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-04 00:29:53 +0300

    Set count to 0

--- js/index.js
+++ js/index.js
@@ -1,31 +1,18 @@
-let countEL = document.getElementById("count-el")
-console.log(countEL)
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
+
+//console.log(saveEl)
+
 function increment() {
-    count = count + 1
-    countEL.innerText = count
+   count += 1
+   countEl.textContent = count //text content replaces innerText because it swallows spaces 
 } 
 
 function save() {
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+        
 }
-
-let username = "Jans"
-let message = "You have 3 new notifications"
-
-console.log(message + ", " + username + "!")
-
-let messageToUser = message + "," + username + "!"
-
-console.log(messageToUser)
-
-let greetings = "Hi, my name is "
-let name = "Maryanne"
-
-let myGreeting = greetings + name 
-console.log(myGreeting)
-
-console.log(4 + 5) //9
-console.log("2" + "4") //24
-console.log("5" + 1) //51
-console.log(100 + "100") //100100
--- js/practise.js
+++ js/practise.js
@@ -34,5 +34,28 @@
 ////
 
 
+///////////
+let username = "Jans"
+let message = "You have 3 new notifications"
+
+console.log(message + ", " + username + "!")
+
+let messageToUser = message + "," + username + "!"
+
+console.log(messageToUser)
+
+/////////
+let greetings = "Hi, my name is "
+let name = "Maryanne"
+
+let myGreeting = greetings + name 
+console.log(myGreeting)
+
+console.log(4 + 5) //9
+console.log("2" + "4") //24
+console.log("5" + 1) //51
+console.log(100 + "100") //100100
+
+///////
 
 
