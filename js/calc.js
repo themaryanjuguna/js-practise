70a9ca006f74c924db4d782abd865b1b8f30194c
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-04 15:49:13 +0300

    Scrimba answer

--- js/calc.js
+++ js/calc.js
@@ -7,23 +7,29 @@ document.getElementById("num2-el").textContent = num2
 // Create four functions: add(), subtract(), divide (), multiply()
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using numi and num2
-function add() {
-    console.log(num1 + num2)
-}
-function subtract() {
-    console.log(num1 - num2)
-}
-function divide() {
-    console.log(num1 / num2)
-}
-function multiply() {
-    console.log(num1 * num2)
-}
-
 // Render the result of the calculation in the paragraph with id="sum-el"
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
-document.getElementById("sum-el").textContent = add()
+
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+}
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+}
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
+}
+
 
 
 
